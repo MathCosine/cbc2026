e0edcbecb76b7125be520506722f0a5737456cfc
MathCosine <mathtomn@gmail.com>
2026-03-19 21:16:23 -0700

    Enhance attack bot with swarm logic and exploration
    Refactor attack bot logic to include swarm targeting and exploration guessing.

--- attack_bot.py
+++ attack_bot.py
@@ -1,33 +1,42 @@
 from cambc import *
+from utils import get_enemy_core_guesses, smart_move
 
 def run_attack_bot(c: Controller, core_pos: Position):
     my_pos = c.get_position()
     my_team = c.get_team()
-    titanium, _ = c.get_global_resources()
-
+    
+    # 1. Survival Check
     if c.get_hp() <= 10:
         nearby = c.get_nearby_units(dist_sq=2)
         for uid in nearby:
             if c.get_team(uid) != my_team:
                 c.self_destruct() 
                 return
-
+    
     if c.get_move_cooldown() > 0:
         return
 
-    dist_from_core = my_pos.distance_squared(core_pos)
-    map_size_sq = (c.get_map_width() / 2) ** 2 + (c.get_map_height() / 2) ** 2
+    # 2. The Marker Network (Swarm Logic)
+    nearby_entities = c.get_nearby_entities(dist_sq=c.get_vision_radius_sq())
+    swarm_target = None
     
-    if dist_from_core > map_size_sq and titanium > c.get_gunner_cost()[0] + 50:
-        if c.get_action_cooldown() == 0:
-            outward_dir = core_pos.direction_to(my_pos)
-            if c.is_tile_empty(my_pos) and c.can_build_gunner(my_pos, outward_dir):
-                c.build_gunner(my_pos, outward_dir)
-                return
+    for uid in nearby_entities:
+        if c.get_entity_type(uid) == EntityType.CORE and c.get_team(uid) != my_team:
+            if c.can_place_marker(my_pos):
+                c.place_marker(my_pos, 999)
+            swarm_target = c.get_position(uid)
+            break
+            
+        elif c.get_entity_type(uid) == EntityType.MARKER and c.get_team(uid) == my_team:
+            if c.get_marker_value(uid) == 999:
+                swarm_target = c.get_position(uid)
 
-    march_dir = core_pos.direction_to(my_pos)
-    if not c.can_move(march_dir):
-        march_dir = march_dir.rotate_right() 
-        
-    if c.can_move(march_dir):
-        c.move(march_dir)
+    if swarm_target:
+        smart_move(c, my_pos, swarm_target)
+        return
+
+    # 3. Exploration (Symmetry Guessing)
+    guesses = get_enemy_core_guesses(core_pos, c.get_map_width(), c.get_map_height())
+    my_guess = guesses[c.get_id() % 3] 
+    
+    smart_move(c, my_pos, my_guess)
