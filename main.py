d24247d22f80fc78f5dc8d4896a0b46be27d7291
MathCosine <mathtomn@gmail.com>
2026-03-19 21:17:36 -0700

    Refactor player bot logic and error handling

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from turret_logic import run_turret
 
 class Player:
     def __init__(self):
+        # Every unit gets its own copy of this variable
         self.core_pos = None
 
     def run(self, c: Controller):
@@ -19,23 +20,36 @@ class Player:
                 run_core(c)
                 
             elif entity_type == EntityType.BUILDER_BOT:
+                # If we don't know where the core is, look around!
+                if self.core_pos is None:
+                    my_team = c.get_team()
+                    nearby_buildings = c.get_nearby_buildings()
+                    
+                    for b_id in nearby_buildings:
+                        if c.get_entity_type(b_id) == EntityType.CORE and c.get_team(b_id) == my_team:
+                            self.core_pos = c.get_position(b_id)
+                            break
+                            
+                # If we STILL don't know (which shouldn't happen), skip turn
                 if self.core_pos is None:
                     return 
 
+                # Now that we know where home is, assign roles and get to work
                 bot_id = c.get_id()
-                role_number = bot_id % 5  # Returns 0, 1, 2, 3, or 4
+                role_number = bot_id % 5  
 
                 if role_number < 3:
-                    run_eco_bot(c, self.core_pos)      # 60% chance
+                    run_eco_bot(c, self.core_pos)      
                 elif role_number == 3:
-                    run_attack_bot(c, self.core_pos)   # 20% chance
+                    run_attack_bot(c, self.core_pos)   
                 else:
-                    run_defense_bot(c, self.core_pos)  # 20% chance
+                    run_defense_bot(c, self.core_pos)  
 
             elif entity_type in (EntityType.GUNNER, EntityType.SENTINEL, EntityType.BREACH):
                 run_turret(c)
 
         except GameError:
+            # We catch game errors so the bot doesn't crash if it tries an illegal move
             pass 
         except Exception as e:
             print(f"Error on unit {c.get_id()}: {e}")
